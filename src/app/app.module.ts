40e0206f2752a48c41d5109709c551a0292d31e0
Matthew Wright <matthewbwright@gmail.com>
2022-09-26 10:34:27 -0700

    adds news, tasks, tools components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TasksComponent } from './tasks/tasks.component';
+import { NewsComponent } from './news/news.component';
+import { ToolsComponent } from './tools/tools.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    NewsComponent,
+    ToolsComponent
   ],
   imports: [
     BrowserModule,
